45954ded0745551884cf2aa3031bbb73248832fa
Manish_Patidar <manish.patidar@webllisto.com>
2022-10-03 16:22:09 +0530

    working on animation page

---
+++ src/component/common/nft-card/index.jsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from './nftcard.module.scss';
+import cardback from '../../../../public/assets/images/card_back.svg';
+
+const NFTCard = () => {
+  return (
+    <div className={`container ${styles.nft_card_wrap}`}>
+      <div className={styles.nft_card_img_wrap}>
+        <div className={styles.backgroud_nft_card}>
+          <Image
+            src={cardback}
+            width={420}
+            height={283}
+            alt="img"
+            layout="fixed"
+          />
+        </div>
+        <div className={styles.img_nft_card}>
+          <Image
+            src={'/assets/images/nft_card.svg'}
+            width={322}
+            height={287}
+            layout="fixed"
+            alt="nft_card"
+          />
+        </div>
+      </div>
+      <div className={styles.nft_card_sec}>
+        <div className={styles.nft_card_art}>
+          <p>Art</p>
+          <h6>Art #1234</h6>
+          <button className={styles.opensea_btn}>View on Opensea</button>
+        </div>
+        <div className={styles.nft_card_price}>
+          <h1>0.207 ETH</h1>
+          <p>$281.21</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NFTCard;
---
+++ src/containers/animation/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Video from '../../component/ui/video';
+import OurArtworks from '../../component/ui/ourartworks';
+import JoinYoutubeChannel from '../../component/ui/join-youtube-channel';
+
+const Animation = () => {
+  return (
+    <>
+      <Video />
+      <OurArtworks />
+      <JoinYoutubeChannel />
+    </>
+  );
+};
+
+export default Animation;
