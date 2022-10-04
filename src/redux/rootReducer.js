4e735ba822ae07960c064865c6aa8fc459012526
Kapil <kapil@weblliasto.com>
2022-10-04 11:54:47 +0530

    intrgrate blog api in home page blog card

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import homeReducer from './home/homeSlice';
+import blogReducer from './blog/blogSlice';
 export const rootReducer = combineReducers({
   homeReducer,
+  blogReducer,
 });
