0d84a8e12e2d3985982d57dafdc8ab9d0dc01c5f
Kapil <kapil@weblliasto.com>
2022-10-06 16:52:23 +0530

    blog data api added

---
+++ pages/blog-detail/[id].js
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+import BlogDetail from '../../src/containers/blog-detail';
+const APP_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const BlogDetailPage = ({ data }) => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div>
+      <BlogDetail id={id} data={data} />
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  const res = await fetch(`${APP_URL}blog/18`);
+  const dataJSON = await res.json();
+  const data = await dataJSON.data;
+
+  return { props: { data } };
+}
+export default BlogDetailPage;
---
+++ src/containers/blog-detail/index.js
@@ -0,0 +1,20 @@
+import moment from 'moment/moment';
+import React from 'react';
+import styles from './blogDetails.module.scss';
+const BlogDetail = ({ id, data }) => {
+  return (
+    <div className={`container ${styles.blog_detail_wrap}`}>
+      {id}
+      <h3 className={styles.blog_heading}>{data.title}</h3>
+      <p className={styles.blog_date}>
+        {moment(data.createdAt).format(' Do MMMM YYYY')}
+      </p>
+      <div
+        className={styles.blog_content}
+        dangerouslySetInnerHTML={{ __html: data.description }}
+      ></div>
+    </div>
+  );
+};
+
+export default BlogDetail;
