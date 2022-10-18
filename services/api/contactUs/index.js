ea62ddd9b5dda504d6c80b3201910e44b6b1339e
Manish_Patidar <manish.patidar@webllisto.com>
2022-10-18 20:31:50 +0530

    implement contact-us user with admin side functionality

---
+++ services/api/contactUs/index.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+const APP_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const getResonForContactApi = async () => {
+  const xhr = await axios.request({
+    method: 'get',
+    url: `${APP_URL}reason-for-contacts`,
+  });
+
+  return xhr.data;
+};
+
+export const insertContactUsFileApi = async (data) => {
+  const xhr = await axios.post(
+    `${APP_URL}upload?fileFor=contactus&fileType=image`,
+    data,
+  );
+  return xhr.data;
+};
+
+export const insertContactUsDetailApi = async (data) => {
+  const xhr = await axios.post(`${APP_URL}contact-us`, data);
+  return xhr.data;
+};
+
+export const getContactUsDataApi = async () => {
+  const xhr = await axios.request({
+    method: 'get',
+    url: `${APP_URL}contact-us`,
+  });
+
+  return xhr.data;
+};
+
+export const contactUsDataOperationApi = async (data) => {
+  const xhr = await axios.request({
+    method: 'get',
+    url: `${APP_URL}contact-us?${data}`,
+  });
+
+  return xhr.data;
+};
+
+export const getcontactUsDataDetailsApi = async (id) => {
+  const xhr = await axios.request({
+    method: 'get',
+    url: `${APP_URL}contact-us/${id}`,
+  });
+
+  return xhr.data;
+};
+
+export const replyByUserApi = async (data) => {
+  const xhr = await axios.post(`${APP_URL}contact-us/reply`, data);
+  return xhr.data;
+};
---
+++ src/component/common/reply-dialoag-box /index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import TextInput from '../text-input';
+import styles from './replyDialogBox.module.scss';
+const ReplyDialogBox = ({
+  leftReplyButtonHandler,
+  rightReplyButtonHandler,
+  handleReplyShow,
+  reply,
+  setReply,
+}) => {
+  return (
+    <div className="dialog_box">
+      {handleReplyShow && (
+        <div className={styles.dialogbox_wrap}>
+          <div className={styles.modal_content}>
+            <div className={styles.modal_header}>
+              <h5 className={styles.dailog_title}>You Can Comment</h5>
+            </div>
+            <p className={styles.dialog_body}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ac
+              nibh ut in. Convallis in tristique dui sit vestibulum habitant
+            </p>
+            <div className={styles.input_wrap}>
+              <TextInput
+                titleBackground={'#AF3277'}
+                handleType={'text'}
+                kind="fullborder"
+                handleValue={reply}
+                handleOnChange={(e) => setReply(e.target.value)}
+                title="Reply"
+              />
+            </div>
+            <div className={styles.dialog_footer}>
+              <button
+                onClick={leftReplyButtonHandler}
+                type="button"
+                className={styles.dialog_left_btn}
+              >
+                Cancel
+              </button>
+              <button
+                onClick={rightReplyButtonHandler}
+                type="button"
+                className={styles.dialog_right_btn}
+              >
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ReplyDialogBox;
