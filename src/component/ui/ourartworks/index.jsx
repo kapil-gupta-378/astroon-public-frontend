9f1c8435c1a123448ebee4ce7bc837503569fa57
Kapil <kapil@weblliasto.com>
2022-10-28 16:17:36 +0530

    animation page slider added to nft

--- src/component/ui/ourartworks/index.jsx
+++ src/component/ui/ourartworks/index.jsx
@@ -5,15 +5,28 @@ import styles from './ourartworks.module.scss';
 import cardBackIcon from '../../../../public/assets/images/forword_icon.svg';
 import cardForwordIcon from '../../../../public/assets/images/backword_icon.svg';
 import Button from '../../common/button';
+import Slider from 'react-slick';
 
 const OurArtworks = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+  };
   return (
     <div className={`container ${styles.nft_card_wrap}`}>
       <h3 className={styles.nft_card_heading}>Our Artworks</h3>
       <div className={styles.nft_cards_wrap}>
-        {[...Array(3).keys()].map((_, idx) => (
-          <NFTCard key={idx} />
-        ))}
+        <Slider {...settings}>
+          {[...Array(8).keys()].map((_, idx) => (
+            <NFTCard key={idx} />
+          ))}
+        </Slider>
       </div>
 
       <div className={styles.nft_card_navigation_btn}>
