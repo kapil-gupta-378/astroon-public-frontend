4c3dc5be8cacd704f7f4607ce47540f7637b7380
amitkushwaha5 <amit.k@webllisto.com>
2022-11-22 16:35:19 +0530

    changed the remember condition

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
-    if (rememberMe || token) {
+    if (rememberMe && adminToken) {
       router.push('admin/admin-management');
       setIsLogin(false);
     } else {
