230972adbbe71a9b852f6e8d1f1e3c9a6fb28691
Kapil <kapil@weblliasto.com>
2022-12-13 17:13:30 +0530

    resques popup design change

--- src/component/common/contact-us/index.jsx
+++ src/component/common/contact-us/index.jsx
@@ -108,153 +108,142 @@ const ContactUs = (props) => {
       aria-labelledby="contained-modal-title-vcenter"
       centered
       className="contact_form_modal"
+      close
     >
+      {' '}
+      <Modal.Header closeVariant={'white'} closeButton></Modal.Header>
       <Modal.Body>
-        <div className={styles.dialogbox_wrap}>
-          <div className={styles.modal_content}>
-            <div className="row">
-              <div className={styles.bg_image}>
-                <p>Astroon / Submit a Request</p>
-                <h3>Submit a Request</h3>
+        <div className={styles.modal_content}>
+          <div className="row">
+            <div className={styles.bg_image}>
+              <p>Astroon / Submit a Request</p>
+              <h3>Submit a Request</h3>
+            </div>
+          </div>
+          <div className={styles.input_wrap}>
+            <TextInput
+              titleBackground={'#ae2e6f'}
+              title={'Email Address'}
+              handleType={'email'}
+              kind="fullborder"
+              placeHolder="Enter your email"
+              handleValue={email}
+              handleOnChange={(e) => setEmail(e.target.value)}
+            />
+
+            <TextInput
+              titleBackground={'#ae2e6f'}
+              title={'Username'}
+              handleType={'text'}
+              kind="fullborder"
+              placeHolder="Enter your username"
+              handleValue={username}
+              handleOnChange={(e) => setUsername(e.target.value)}
+            />
+
+            <FormSelect
+              titleBackground={'#ae2e6f'}
+              label={'Reason for contact'}
+              options={getReason}
+              handleChange={(value) => setReasonForContact(value.value)}
+            />
+
+            <TextInput
+              titleBackground={'#ae2e6f'}
+              title={'Subject'}
+              handleType={'text'}
+              kind="fullborder"
+              placeHolder="Enter your Subject"
+              handleValue={subject}
+              handleOnChange={(e) => setSubject(e.target.value)}
+            />
+            <TextInput
+              titleBackground={'#ae2e6f'}
+              title={'Description'}
+              handleType={'textarea'}
+              kind="fullborder"
+              placeHolder="Enter your description"
+              handleValue={description}
+              textarea={true}
+              inputHeight={'120px'}
+              handleOnChange={(e) => setDescription(e.target.value)}
+            />
+
+            <div className={inputStyles.input_wrap}>
+              <div
+                className={inputStyles.corner_border_label}
+                style={{ background: '#ae2e6f' }}
+              >
+                <label>Attachments</label>
+              </div>
+
+              <div
+                style={{
+                  padding: 0,
+                  width: '100%',
+                  overflow: 'hidden',
+                  cursor: 'pointer',
+                  display: 'inline-block',
+                }}
+                className={inputStyles.full_border}
+              >
+                <div
+                  style={{
+                    cursor: 'pointer',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <p
+                    style={{
+                      cursor: 'pointer',
+                      marginBottom: 0,
+                      padding: '8px',
+                      opacity: '0.5',
+                      marginLeft: '19px',
+                    }}
+                  >
+                    {attachment
+                      ? attachment.name
+                      : 'Add file or drop files here'}
+                  </p>
+                  <h6
+                    style={{
+                      background: 'white',
+                      color: '#020039',
+                      fontSize: '14px',
+                      float: 'right',
+                      padding: '14px 20px',
+                      marginBottom: 0,
+                      cursor: 'pointer',
+                    }}
+                  >
+                    {attachment ? 'Change File' : 'Upload File'}
+                  </h6>
+                </div>
+                <input
+                  style={{
+                    height: '68px',
+                    cursor: 'pointer',
+                    fontSize: '100px',
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    opacity: 0,
+                  }}
+                  type="file"
+                  className={inputStyles.full_border}
+                  placeholder="Upload File"
+                  onChange={uploadAttachment}
+                  accept="image/png, image/gif, image/jpeg"
+                />
               </div>
             </div>
-            <div
-              className="row"
-              style={{
-                marginTop: '120px',
-              }}
-            >
-              <div className={styles.input_wrap}>
-                <TextInput
-                  title={'Email Address'}
-                  handleType={'email'}
-                  kind="fullborder"
-                  placeHolder="Enter your email"
-                  handleValue={email}
-                  handleOnChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <TextInput
-                  title={'Username'}
-                  handleType={'text'}
-                  kind="fullborder"
-                  placeHolder="Enter your username"
-                  handleValue={username}
-                  handleOnChange={(e) => setUsername(e.target.value)}
-                />
-                <br />
-                <FormSelect
-                  label={'Reason for contact'}
-                  options={getReason}
-                  handleChange={(value) => setReasonForContact(value.value)}
-                />
-                <br />
-                <TextInput
-                  title={'Subject'}
-                  handleType={'text'}
-                  kind="fullborder"
-                  placeHolder="Enter your Subject"
-                  handleValue={subject}
-                  handleOnChange={(e) => setSubject(e.target.value)}
-                />
-                <br />
-                <div className={inputStyles.input_wrap}>
-                  <label
-                    className={inputStyles.corner_border_label}
-                    style={{ background: 'rgb(5, 5, 45)' }}
-                  >
-                    Description
-                  </label>
-                  <textarea
-                    placeholder="Enter your description"
-                    className={styles.contact_text_area}
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    rows={5}
-                    cols={4}
-                  />
-                </div>
-                <br />
-                <div className={inputStyles.input_wrap}>
-                  <div
-                    className={inputStyles.corner_border_label}
-                    style={{ background: 'rgb(5, 5, 45)' }}
-                  >
-                    <label>Attachments</label>
-                  </div>
-
-                  <div
-                    style={{
-                      padding: 0,
-                      width: '100%',
-                      overflow: 'hidden',
-                      cursor: 'pointer',
-                      display: 'inline-block',
-                    }}
-                    className={inputStyles.full_border}
-                  >
-                    <div
-                      style={{
-                        cursor: 'pointer',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                      }}
-                    >
-                      <p
-                        style={{
-                          cursor: 'pointer',
-                          marginBottom: 0,
-                          padding: '8px',
-                          opacity: '0.5',
-                        }}
-                      >
-                        {attachment
-                          ? attachment.name
-                          : 'Add file or drop files here'}
-                      </p>
-                      <h6
-                        style={{
-                          background:
-                            'linear-gradient(141.07deg, #FAFF00 -6.88%, #FE19C1 50.39%)',
-                          float: 'right',
-                          padding: '14px 30px',
-                          marginBottom: 0,
-                          cursor: 'pointer',
-                        }}
-                      >
-                        {attachment ? 'Change File' : 'Upload File'}
-                      </h6>
-                    </div>
-                    <input
-                      style={{
-                        height: '68px',
-                        cursor: 'pointer',
-                        fontSize: '100px',
-                        position: 'absolute',
-                        left: 0,
-                        top: 0,
-                        opacity: 0,
-                      }}
-                      type="file"
-                      className={inputStyles.full_border}
-                      placeholder="Upload File"
-                      onChange={uploadAttachment}
-                      accept="image/png, image/gif, image/jpeg"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className={styles.dialog_footer}>
-                <button
-                  onClick={props.onHide}
-                  type="button"
-                  className={styles.cancel_btn}
-                >
-                  Cancel
-                </button>{' '}
-                <Button onClick={submitContactDetails}>Submit a request</Button>
-              </div>
+            <div className={styles.dialog_footer}>
+              <Button kind={'white_btn'} onClick={submitContactDetails}>
+                Submit a request
+              </Button>
             </div>
           </div>
         </div>
