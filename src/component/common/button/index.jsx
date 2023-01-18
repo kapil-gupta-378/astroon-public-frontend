1de111cb887d794cab2a6e95bb2119163b876dc0
Kapil <kapil@weblliasto.com>
2023-01-18 16:00:09 +0530

    change in UI #2

--- src/component/common/button/index.jsx
+++ src/component/common/button/index.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './button.module.scss';
-import axios from 'axios';
+// import axios from 'axios';
 
 const Button = ({ children, kind, ...rest }) => {
   const downloadFile = () => {
-    axios
-      .get(
-        'https://www.tutorialspoint.com/website_development/website_development_tutorial.pdfd',
-      )
-      .then((response) => {
-        // Create blob link to download
-        const url = window.URL.createObjectURL(new Blob([response]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', `FileName.pdf`);
-        // Append to html link element page
-        document.body.appendChild(link);
-        // Start download
-        link.click();
-        // Clean up and remove the link
-        link.parentNode.removeChild(link);
-      });
+    window.open(
+      'https://file-examples.com/storage/fe2879c03363c669a9ef954/2017/10/file-sample_150kB.pdf',
+      '_blank',
+    );
+    // axios
+    //   .get(
+    //     'https://file-examples.com/storage/fe2879c03363c669a9ef954/2017/10/file-sample_150kB.pdf',
+    //   )
+    //   .then((response) => {
+    //     // Create blob link to download
+    //     const url = window.URL.createObjectURL(new Blob([response]));
+    //     const link = document.createElement('a');
+    //     link.href = url;
+    //     link.setAttribute('download', `FileName.pdf`);
+    //     // Append to html link element page
+    //     document.body.appendChild(link);
+    //     // Start download
+    //     link.click();
+    //     // Clean up and remove the link
+    //     link.parentNode.removeChild(link);
+    //   });
   };
 
   return (
