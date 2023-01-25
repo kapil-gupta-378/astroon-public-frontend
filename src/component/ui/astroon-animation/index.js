e4d03f7ff0ea0324117a4ded436b15324ed90a90
Kapil <kapil@weblliasto.com>
2023-01-25 16:11:50 +0530

    UI changes & nft preSale contract address change

--- src/component/ui/astroon-animation/index.js
+++ src/component/ui/astroon-animation/index.js
@@ -7,7 +7,7 @@ const AstroonAnimation = () => {
     <div className={`container ${styles.astroon_animation_wrap}`}>
       <h3 className={styles.astroon_animation_heading}>ASTROON Animation</h3>
       <div className={styles.content_wrap}>
-        <div className={styles.left_wrap}>
+        {/* <div className={styles.left_wrap}>
           <p>
             {
               "An animated series suitable for all age audiences will be distributed on the world's largest streaming platforms.           Will Astro and Road be able to go to the moon? We don't know yet, but we trust them.                   Episodes of this adventure will be posted weekly on YouTube. Don't forget to support them by watching their journey."
@@ -32,7 +32,7 @@ const AstroonAnimation = () => {
               </li>
             </ol>
           </div>
-        </div>
+        </div> */}
 
         <div className={styles.right_wrap}>
           <Image
